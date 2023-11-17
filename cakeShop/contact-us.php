c9b547d2fca1c923bbb245ef089941e1c91c31e5
Chawakorn Chaimongkol <107240946+R4p1Z@users.noreply.github.com>
2023-11-17 23:24:50 +0700

    Contact-us & style added
    Adding contact-us page and adding more style for contact-us page.

---
+++ cakeShop/contact-us.php
@@ -0,0 +1,29 @@
+<?php 
+session_start();
+require_once 'config/connect.php';
+include 'inc/header.php'; ?>
+<?php include 'inc/nav.php'; ?>
+
+  <!-- SHOP CONTENT -->
+  <section id="content">
+    <div class="content-blog">
+      <div class="container">
+        <div class="row">
+        <div class="page_header">
+          <h1>Contact Us</h1>
+        </div>
+
+        <div class="left-half">
+          <h2>LINE ID:</h2>
+        </div>
+        <div class="left-half">
+          <p>Tiktok:</p>
+        </div>
+        <div class="left-half">
+          <p>Facebook:</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php include 'inc/footer.php' ?>
