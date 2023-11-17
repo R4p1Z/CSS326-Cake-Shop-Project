a67ed88cc72eecf34ac1afa94106e1be745eaf15
Chawakorn Chaimongkol <107240946+R4p1Z@users.noreply.github.com>
2023-11-17 23:46:55 +0700

    Text size & alignment adjust
    Like the header said.

--- cakeShop/contact-us.php
+++ cakeShop/contact-us.php
@@ -12,15 +12,23 @@ include 'inc/header.php'; ?>
         <div class="page_header">
           <h1>Contact Us</h1>
         </div>
-
+          <!-- DO the column width:50% -->
         <div class="left-half">
           <h2>LINE ID:</h2>
+          <h2>X:</h2>
+        </div>
+
+        <div class="left-half">
+          <h2>Tiktok:</h2>
         </div>
         <div class="left-half">
-          <p>Tiktok:</p>
+          <h2>Facebook:</h2>
         </div>
-        <div class="left-half">
-          <p>Facebook:</p>
+        <div class="right-half">
+          <h2>Instagram:</h2>
+        </div>
+        <div class="right-half">
+          <h2>Phone numbers:</h2>
         </div>
       </div>
     </div>
