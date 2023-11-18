af9278fa7018db3dbcf2a87b092e94c0b493d5a0
Chawakorn Chaimongkol <107240946+R4p1Z@users.noreply.github.com>
2023-11-18 19:44:26 +0700

    about us page by Hugo
    adding about us page & pic

---
+++ cakeShop/aboutus.php
@@ -0,0 +1,40 @@
+<?php 
+session_start();
+require_once 'config/connect.php';
+include 'inc/header.php'; ?>
+<?php include 'inc/nav.php'; ?>
+	
+	<!-- SHOP CONTENT -->
+	<section id="content">
+		<div class="content-blog">
+			<div class="container">
+				<div class="row">
+					<div class="page_header text-center">
+						<h2>About Us</h2>
+						<p>Our story and where the magic begin</p>
+                        <div>
+                        <p style="display:inline-block;">
+                        <img src="pic/about.png">
+                        <h3>Welcome to HPY cake, your go-to cake shop nestled in the heart of Bangkok, Thailand. We pride ourselves on delivering an extraordinary cake experience, marked by freshness and premium quality.
+
+                        Our shop, located in the bustling city of Bangkok, is dedicated to creating cakes that are not just desserts but works of art. Each cake is a testament to our commitment to providing moments of sweetness for your special occasions.
+
+                        At HPY cake, the journey to exceptional quality begins with the careful selection of the finest ingredients. We believe in using only the best, ensuring that every bite of our cakes is a burst of flavor and freshness.
+
+                        Whether you're celebrating a birthday, wedding, anniversary, or any other special moment, our cakes are designed to elevate your experience. From classic designs to custom creations, our diverse collection caters to a variety of tastes and styles.
+
+                        What sets us apart is our dedication to the art of baking. Our skilled team of bakers and decorators pour their passion and expertise into every creation. When you step into our shop, the aroma of freshly baked goods welcomes you, and our friendly staff is ready to assist you in finding the perfect cake.
+
+                        As a local business in Bangkok, we take pride in being a part of the community. We invite you to visit [Your Shop Name], where we aim to create joyous moments one slice at a time.
+
+                        Thank you for choosing HPY cake for your cake needs. We look forward to serving you and being a part of your celebrations.</h3>
+
+                        </p>
+                        
+					
+						<div class="clearfix"></div>					
+				</div>
+			</div>
+		</div>
+	</section>
+<?php include 'inc/footer.php' ?>
